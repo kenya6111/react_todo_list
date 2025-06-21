d0a94415bf815fab658e636e094169fbbf2ac8b9
kenya6111 <kenyanke6111@gmail.com>
2025-06-21 16:57:33 +0900

    placeholderのprops削除

--- src/components/molecules/SearchInput.tsx
+++ src/components/molecules/SearchInput.tsx
@@ -1,13 +1,13 @@
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { Input } from "../atoms/input/Input";
 export const SearchInput = (props) => {
-  const { inputTodo, onChangeTodo, placeholder, onClickSave } = props;
+  const { inputTodo, onChangeTodo, onClickSave } = props;
   return (
     <>
       <Input
         value={inputTodo}
         onChange={onChangeTodo}
-        placeholder={placeholder}
+        placeholder="タスクを入力"
       />
       <PrimaryButton onClick={onClickSave}>保存</PrimaryButton>
     </>
